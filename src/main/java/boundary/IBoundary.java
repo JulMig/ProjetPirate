ad0c5fdf358f2ba0770d4110ab99fcbdfd9577c5
Mariama DIALLO <diallo.mbobo108@gmail.com>
2024-05-11 15:23:26 +0200

    IBoundary

--- src/main/java/boundary/IBoundary.java
+++ src/main/java/boundary/IBoundary.java
@@ -1,7 +1,20 @@
 
 package boundary;
 
+import entity.Joueur;
+import controller.ILancerDe;
 
 public interface IBoundary {
+	
+	public String afficherJoueur(Joueur joueurCourant);
+	
+	public void setILancerDe (ILancerDe IDeplacerPirate);
+	
+	public int lancerDes();
+	
+	public void setDeplacerPirate(ILancerDe IDeplacerPirate);
+	
+	
+	
     
 }
