0eff9c9f4f04eb8eefe02cf704400e7d0bc20540
JulMig <julie.mignotte8@gmail.com>
2024-05-11 16:09:46 +0200

    Ajout de constructeur jeu et joueur

--- src/main/java/entity/Jeu.java
+++ src/main/java/entity/Jeu.java
@@ -3,6 +3,11 @@ package entity;
 public class Jeu {
     private Joueur [] joueurs = new Joueur[2];
     private int idJoueurCourant = 0;
+
+    public Jeu(Joueur joueur1, Joueur joueur2) {
+        joueurs[0] = joueur1;
+        joueurs[1] = joueur2;
+    }
     
     
    public Joueur getJoueurCourant(){
--- src/main/java/entity/Joueur.java
+++ src/main/java/entity/Joueur.java
@@ -3,6 +3,10 @@ package entity;
 public class Joueur {
     String nom;
 
+    public Joueur(String nom) {
+        this.nom = nom;
+    }
+    
     public String getNom() {
         return nom;
     }
