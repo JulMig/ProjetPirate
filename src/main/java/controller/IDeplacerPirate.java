287db269b2536eaa51f363b7844ccbf91149e529
dina-ups <112731912+dina-ups@users.noreply.github.com>
2024-05-11 15:45:14 +0200

    Add files via upload
    interfaces_controller

--- src/main/java/controller/IDeplacerPirate.java
+++ src/main/java/controller/IDeplacerPirate.java
@@ -1,5 +1,14 @@
 package controller;
 
+import entity.*;
+/**
+ *
+ * @author Julie Mignotte
+ */
+
 public interface IDeplacerPirate {
+    public int deplacerPirate(Joueur joueur);
+    public boolean verifierDeplacement(Joueur joueur);
+    
     
 }
