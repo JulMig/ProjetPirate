8cd47436b2ce33dfb167db0ef1931de086a3fe6b
DialloBobo <114510211+dialloBobo@users.noreply.github.com>
2024-05-11 17:08:13 +0200

    Add files via upload
    joueur completer

--- src/main/java/entity/Joueur.java
+++ src/main/java/entity/Joueur.java
@@ -12,6 +12,19 @@ public class Joueur {
     public String getNom() {
         return nom;
     }
+    public int deplacerPirate(int nbPas) {
+        // Déplacer le pirate en ajoutant le nombre de pas à la position actuelle
+        this.positionCourante += nbPas;
+        return this.positionCourante; // Retourne la nouvelle position du pirate c'est num case
+        
+    }
+    
+    public boolean verifierDeplacement(int nbPas, int nbPasFait){
+        int new_Position = this.positionCourante + nbPasFait;
+           
+        return (new_Position >= 0) && (new_Position <= nbPas);
+        }
+    
     
     
 }
