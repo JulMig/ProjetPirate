1067fa1b94968842d97806f55dda7e1ffca55f8d
DialloBobo <114510211+dialloBobo@users.noreply.github.com>
2024-05-11 16:36:25 +0200

    Add files via upload
    lancerDes

--- src/main/java/entity/De.java
+++ src/main/java/entity/De.java
@@ -6,8 +6,12 @@ import java.util.Random;
 public class De {
     private static Random random = new Random();
     
-    public static int lancerDe(){
-        return (random.nextInt(6))+1;
+    public static int[] lancerDes(int nombreDes) {
+        int[] resultats = new int[nombreDes];
+        for (int i = 0; i < nombreDes; i++) {
+            resultats[i] = random.nextInt(6) + 1;
+        }
+        return resultats;
     }
     
 }
