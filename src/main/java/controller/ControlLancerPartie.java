7d5db6e0611560c799695fd186aa5d103d86877c
DialloBobo <114510211+dialloBobo@users.noreply.github.com>
2024-05-11 16:15:23 +0200

    Add files via upload
    initilaiserPartie

--- src/main/java/controller/ControlLancerPartie.java
+++ src/main/java/controller/ControlLancerPartie.java
@@ -1,7 +1,28 @@
 
 package controller;
 
+import entity.Jeu;
+import entity.Joueur;
 
 public class ControlLancerPartie {
+	
+	
+	
+	 public static Jeu initialiserJeu() {
+	        // Création des joueurs
+	        Joueur joueur1 = new Joueur("joueur1");
+	        Joueur joueur2 = new Joueur("joueur2");
+	        
+	        
+	        Joueur[] joueurs = {joueur1, joueur2};
+	        
+	        
+	        return new Jeu(joueurs);
+	    }
+
+	
+	
+	
+	
     
 }
