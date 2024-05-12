04f31e947de6363ac6bce3a2c1137faea793e91f
DialloBobo <114510211+dialloBobo@users.noreply.github.com>
2024-05-12 13:09:05 +0200

    Add files via upload

--- src/main/java/controller/ControlVerifierFin.java
+++ src/main/java/controller/ControlVerifierFin.java
@@ -13,7 +13,28 @@ public class ControlVerifierFin implements IVerifierFin {
     }
     
     public boolean verifierFin(){
-        //TODO
-        return false;
+    	
+    	
+        boolean joueur1_nul = joueur1.getVie() == 0;
+        boolean joueur2_nul = joueur2.getVie() == 0;
+        
+        if (joueur1_nul) {
+            System.out.println(joueur2.getNom() + " a gagné la partie !");
+            return true; 
+        } else if (joueur2_nul) {
+            System.out.println(joueur1.getNom() + " a gagné la partie !");
+            return true; 
+        }
+        
+        
+        if (joueur1.getPositionCourante() == 30) {
+            System.out.println(joueur1.getNom() + " a trouvé la barque et devient le capitaine !");
+            return true; 
+        } else if (joueur2.getPositionCourante() == 30) {
+            System.out.println(joueur2.getNom() + " a trouvé la barque et devient le capitaine !");
+            return true; 
+        }
+        
+        return false; // La partie continue
     }
 }
